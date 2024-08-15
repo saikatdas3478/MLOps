5519a728167cbc5562fcd7d05d7cf7e215a00249
Saikat Das <saikatdas3478@gmail.com>
2024-08-16 00:33:49 +0530

    Added a new function named print_name

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ def hello_world():
     
     print('Hello World')
     
+def print_name(name):
+    
+    print(f'Hello {name}')
+    
 if __name__ == '__main__':
     hello_world()
+    
+    print_name('Saikat')
